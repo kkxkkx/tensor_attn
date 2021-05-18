3c11fa9446375ec797442fed55c22c00dfd1a65c
kkxkkx <445359738@qq.com>
2021-05-18 14:40:45 +0800

    注释

--- layers_tf/decoder.py
+++ layers_tf/decoder.py
@@ -179,27 +179,27 @@ class BaseRNNDecoder(tf.keras.Model):
         return embed
 
 
-class BahdanauAttention(tf.keras.Model):
-    def __init__(self, units):
-        super(BahdanauAttention, self).__init__()
-        self.W1 = tf.keras.layers.Dense(units)
-        self.W2 = tf.keras.layers.Dense(units)
-        self.V = tf.keras.layers.Dense(1)
-
-    def call(self, query, values):
-        # value是编码器中输出的结果
-        # query是编码器中输出的隐层向量
-        hidden_with_time_axis = tf.expand_dims(query, 1)
-
-        score = self.V(tf.nn.tanh(
-            self.W1(values) + self.W2(hidden_with_time_axis)))
-
-        attention_weights = tf.nn.softmax(score, axis=1)
-
-        context_vector = attention_weights * values
-        context_vector = tf.reduce_sum(context_vector, axis=1)
-
-        return context_vector, attention_weights
+# class BahdanauAttention(tf.keras.Model):
+#     def __init__(self, units):
+#         super(BahdanauAttention, self).__init__()
+#         self.W1 = tf.keras.layers.Dense(units)
+#         self.W2 = tf.keras.layers.Dense(units)
+#         self.V = tf.keras.layers.Dense(1)
+#
+#     def call(self, query, values):
+#         # value是编码器中输出的结果
+#         # query是编码器中输出的隐层向量
+#         hidden_with_time_axis = tf.expand_dims(query, 1)
+#
+#         score = self.V(tf.nn.tanh(
+#             self.W1(values) + self.W2(hidden_with_time_axis)))
+#
+#         attention_weights = tf.nn.softmax(score, axis=1)
+#
+#         context_vector = attention_weights * values
+#         context_vector = tf.reduce_sum(context_vector, axis=1)
+#
+#         return context_vector, attention_weights
 
 
 class DecoderRNN(BaseRNNDecoder):
