6667d40400a85735b8b019bf8a11ba40ebb8c162
kkxkkx <445359738@qq.com>
2021-05-19 10:55:56 +0800

    z

--- layers_tf/decoder.py
+++ layers_tf/decoder.py
@@ -263,7 +263,8 @@ class DecoderRNN(BaseRNNDecoder):
         # x=[271,] -> [271,300]
         x = self.embed(x)
         # x=[271,1,300]
-        x = tf.expand_dims(x, 1, 1)
+        x = tf.expand_dims(x, 1)
+        context_vector = tf.expand_dims(x, 1)
         # x= [271,1,812]
         x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
         print('x')
