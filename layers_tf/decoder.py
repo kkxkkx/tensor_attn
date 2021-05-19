65a1684456d510f03a726a48492d0fcf41b46851
kkxkkx <445359738@qq.com>
2021-05-19 10:15:32 +0800

    tensor不能concat

--- layers_tf/decoder.py
+++ layers_tf/decoder.py
@@ -259,7 +259,9 @@ class DecoderRNN(BaseRNNDecoder):
         # x=[271,] -> [271,300]
         # x = self.embed(x)
         # x=[271,1,300]
-        # x = tf.expand_dims(x, 1)
+        x = tf.expand_dims(x, 1)
+        print('x')
+        print(x.shape)
         # x= [271,1,812]
         x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
         print('x')
