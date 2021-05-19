e3b30adbfb58fac9501211cd1a119486786142cb
kkxkkx <445359738@qq.com>
2021-05-19 14:34:56 +0800

    目前无bug，可以运行一个iter

--- layers_tf/decoder.py
+++ layers_tf/decoder.py
@@ -254,11 +254,11 @@ class DecoderRNN(BaseRNNDecoder):
         # context=[32,512]
         # axis是轴的数字，本来应该是1 ，为了匹配维度，暂时修改成0
         # context_vector = tf.reduce_sum(context_vector, axis=0)
-        print(context_vector.shape)
+        # print(context_vector.shape)
         # x: [batch_size] => [batch_size, hidden_size]
         # x=[271,] -> [271,300]
         x = self.embed(x)
-        print(x.shape)
+        # print(x.shape)
         # x=[271,1,300]
         # x = tf.expand_dims(x, 1)
         # x = [271,821]
