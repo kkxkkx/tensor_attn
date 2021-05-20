3055962392dbf6791968bec35477d1891332a023
kkxkkx <445359738@qq.com>
2021-05-20 09:45:39 +0800

    修beam中的bug

--- models_tf.py
+++ models_tf.py
@@ -1,6 +1,5 @@
-
 from utils_tf import Vocab
-from utils_tf import pad,  bag_of_words_loss, to_bow, EOS_ID
+from utils_tf import pad, bag_of_words_loss, to_bow, EOS_ID
 import layers_tf
 import tensorflow as tf
 from configs_tf import get_config
@@ -9,49 +8,50 @@ import numpy as np
 
 VariationalModels = ['VHRED', 'VHCR']
 
+
 class HRED(tf.keras.Model):
     def __init__(self, config):
         super(HRED, self).__init__()
 
         self.config = config
         self.encoder = layers_tf.EncoderRNN(config.vocab_size,
-                                         config.embedding_size,
-                                         config.encoder_hidden_size,
-                                         dropout=config.dropout,
-                                         bidirectional=config.bidirectional
-                                        )
+                                            config.embedding_size,
+                                            config.encoder_hidden_size,
+                                            dropout=config.dropout,
+                                            bidirectional=config.bidirectional
+                                            )
         context_input_size = (config.num_layers
                               * config.encoder_hidden_size
                               * self.encoder.num_directions)
 
         self.image_encoder = layers_tf.ImageEncoder(context_input_size)
 
-        self.context_encoder = layers_tf.ContextRNN(context_input_size*2,
-                                                 config.context_size,
-                                                 num_layers=config.num_layers,
-                                                 dropout=config.dropout)
+        self.context_encoder = layers_tf.ContextRNN(context_input_size * 2,
+                                                    config.context_size,
+                                                    num_layers=config.num_layers,
+                                                    dropout=config.dropout)
 
         self.decoder = layers_tf.DecoderRNN(config.vocab_size,
-                                         config.embedding_size,
-                                         config.decoder_hidden_size,
-                                         num_layers=config.num_layers,
-                                         dropout=config.dropout,
-                                         word_drop=config.word_drop,
-                                         max_unroll=config.max_unroll,
-                                         sample=config.sample,
-                                         temperature=config.temperature,
-                                         beam_size=config.beam_size
-                                        )
+                                            config.embedding_size,
+                                            config.decoder_hidden_size,
+                                            num_layers=config.num_layers,
+                                            dropout=config.dropout,
+                                            word_drop=config.word_drop,
+                                            max_unroll=config.max_unroll,
+                                            sample=config.sample,
+                                            temperature=config.temperature,
+                                            beam_size=config.beam_size
+                                            )
         self.attention_type = 'Bahdanau'
-        self.feedforward = tf.keras.layers.Dense(self.config.decoder_hidden_size,self.config.activation)
+        self.feedforward = tf.keras.layers.Dense(self.config.decoder_hidden_size, self.config.activation)
 
         if config.tie_embedding:
             self.decoder.embedding = self.encoder.embedding
 
     def call(self, input_sentences, input_sentence_length,
-                input_conversation_length, target_sentences,
-                input_images, input_images_length,
-                decode=False):
+             input_conversation_length, target_sentences,
+             input_images, input_images_length,
+             decode=False):
         """
         Args:
             input_sentences: (Variable, LongTensor) [num_sentences, seq_len]
@@ -65,11 +65,11 @@ class HRED(tf.keras.Model):
         max_len = int(tf.reduce_max(input_conversation_length))
 
         batch_size = input_conversation_length.shape[0]
-        _, encoder_hidden = self.encoder(input_sentences,input_sentence_length)
+        _, encoder_hidden = self.encoder(input_sentences, input_sentence_length)
 
         input_images = tf.reshape(input_images, [batch_size, -1, 3, 224, 224])
         input_images_length = tf.reshape(input_images_length, [batch_size, -1])
-        input_images = tf.slice(input_images, [0]*5, [-1, max_len]+[-1]*3)
+        input_images = tf.slice(input_images, [0] * 5, [-1, max_len] + [-1] * 3)
         input_images = tf.reshape(input_images, [-1, 224, 224, 3])
         input_images_length = tf.slice(input_images_length, [0, 0], [-1, max_len])
 
@@ -80,14 +80,14 @@ class HRED(tf.keras.Model):
 
         if self.encoder.bidirectional:
             encoder_hidden = tf.stack(encoder_hidden, axis=1)
-        encoder_hidden = tf.reshape(encoder_hidden, [num_sentences,-1])
+        encoder_hidden = tf.reshape(encoder_hidden, [num_sentences, -1])
 
         start = tf.cumsum(tf.concat((tf.convert_to_tensor([0], dtype=tf.float32), input_conversation_length[:-1]), 0))
 
         # encoder_hidden: [batch_size, max_len, num_layers * direction * hidden_size]
-        encoder_hidden = tf.stack([pad(encoder_hidden[int(s):int(s+l)], max_len)
-                                      for s, l in zip(start, input_conversation_length)], 0)
-        #comb_encoder_hidden=[32,9,2048]
+        encoder_hidden = tf.stack([pad(encoder_hidden[int(s):int(s + l)], max_len)
+                                   for s, l in zip(start, input_conversation_length)], 0)
+        # comb_encoder_hidden=[32,9,2048]
         comb_encoder_hidden = tf.concat([encoder_hidden, img_encoder_outputs], 2)
         # max_len=9
         # context_outputs=[32,9,512]
@@ -99,8 +99,8 @@ class HRED(tf.keras.Model):
                                      for i, l in enumerate(input_conversation_length)], 0)
         # context_output=[271,512]
         context_outputs = self.feedforward(context_outputs)
-        #decoder_init=[1,275,512]
-        decoder_init = tf.reshape(context_outputs,[self.decoder.num_layers, -1, self.decoder.hidden_size])
+        # decoder_init=[1,275,512]
+        decoder_init = tf.reshape(context_outputs, [self.decoder.num_layers, -1, self.decoder.hidden_size])
         # print(decoder_init.shape)
         if not decode:
             decoder_outputs = self.decoder(target_sentences,
@@ -109,7 +109,9 @@ class HRED(tf.keras.Model):
                                            decode=decode)
             return decoder_outputs
         else:
-            prediction, final_score, length = self.decoder.beam_decode(init_h=decoder_init)
+            # 添加了encoder_outputs
+            prediction, final_score, length = self.decoder.beam_decode(encoder_outputs=context_outputs,
+                                                                       init_h=decoder_init)
             return prediction
 
     def generate(self, context, sentence_length, n_context, input_images, input_images_length):
@@ -123,9 +125,9 @@ class HRED(tf.keras.Model):
         for i in range(n_context):
             # encoder_outputs: [batch_size, seq_len, hidden_size * direction]
             # encoder_hidden: [num_layers * direction, batch_size, hidden_size]
-            encoder_outputs, encoder_hidden = self.encoder(context,sentence_length)
+            encoder_outputs, encoder_hidden = self.encoder(context, sentence_length)
 
-            encoder_hidden = tf.reshape(tf.transpose(encoder_hidden,[1, 0,2]), [batch_size, -1])
+            encoder_hidden = tf.reshape(tf.transpose(encoder_hidden, [1, 0, 2]), [batch_size, -1])
 
             input_image = tf.expand_dims(input_images[i], 0)
             img_encoder_outputs = self.image_encoder(input_image)
@@ -142,7 +144,7 @@ class HRED(tf.keras.Model):
             decoder_init = tf.reshape(context_outputs, [self.decoder.num_layers, -1, self.decoder.hidden_size])
             prediction, final_score, length = self.decoder.beam_decode(init_h=decoder_init)
             # prediction: [batch_size, seq_len]
-            prediction = prediction[:, 0, :] #选最好的
+            prediction = prediction[:, 0, :]  # 选最好的
             # length: [batch_size]
             length = [l[0] for l in length]
             length = tf.convert_to_tensor(length)
@@ -171,12 +173,12 @@ if __name__ == '__main__':
     config.vocab_size = vocab.vocab_size
     model = HRED(config)
 
-    sent = tf.zeros([151,50])
-    tarsent = tf.zeros([151,50])
-    sent_len = tf.convert_to_tensor([4]*151)
-    conv_len = tf.convert_to_tensor([2,2,2,2,3,3,3,3]*4)
-    img = tf.convert_to_tensor(np.random.random([320,3,244,244]))
-    img_len = tf.convert_to_tensor([0,1]*160)
+    sent = tf.zeros([151, 50])
+    tarsent = tf.zeros([151, 50])
+    sent_len = tf.convert_to_tensor([4] * 151)
+    conv_len = tf.convert_to_tensor([2, 2, 2, 2, 3, 3, 3, 3] * 4)
+    img = tf.convert_to_tensor(np.random.random([320, 3, 244, 244]))
+    img_len = tf.convert_to_tensor([0, 1] * 160)
 
     logit = model(sent, sent_len, conv_len, tarsent, img, img_len)
     print(logit.shape)
